014d77d7094523c4fed5249beb4f84863bf4b4a9
jeunseon <euns420@naver.com>
2024-08-21 21:09:08 +0900

    visual 영역 logoWrap gsap scrollTrigger

--- js/main.js
+++ js/main.js
@@ -4,6 +4,31 @@ window.onload = function(){
     menuOpen.addEventListener('click', () => {
         menuBox.classList.toggle('on')
     });
+    // menu click scroll
+    const menuList = document.querySelector('.menuBox li a');
+
+    // top 버튼
+    const topBtn = document.querySelector('.top');
+    topBtn.addEventListener('click', () => {
+        $('body, html').animate({scrollTop:0}, 400);
+        return false;
+    });
 
 
+    gsap .registerPlugin(ScrollTrigger);
+    gsap.timeline({
+        scrollTrigger: {
+            trigger: '.visual',
+            start:'100% 100%',
+            end:'100% 0%',
+            scrub:1,
+            markers: true
+        }
+    })
+    .to('.logoWrap #j', {x: -150, y: 350, rotate: -10, ease: 'none', duration: 5}, 0)
+    .to('.logoWrap #e_1', {x: 0, y: 400, rotate: -10, ease: 'none', duration: 5}, 0)
+    .to('.logoWrap #s', {x: 50, y: 300, rotate: 10, ease: 'none', duration: 5}, 0)
+    .to('.logoWrap #e_2', {x: 30, y: 430, rotate: 20, ease: 'none', duration: 5}, 0)
+    .to('.logoWrap #o', {x: 100, y: 290, rotate: -10, ease: 'none', duration: 5}, 0)
+    .to('.logoWrap #n', {x: 50, y: 450, rotate: 20, ease: 'none', duration: 5}, 0)
 }
